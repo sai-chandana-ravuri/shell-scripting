6f0d2d9839ad1474d6972ecc02c1110f7ee94882
sai-chandana-ravuri <saichandanaravuri@gmail.com>
2026-04-17 22:38:25 +0530

    code-changes

--- 08-datatypes.sh
+++ 08-datatypes.sh
@@ -8,6 +8,6 @@ TOTAL=$(($NUM1+$NUM2))
 TOTAL1=$(($NUM1+$NUM2+$NUM3))
 echo "Total sum is $TOTAL"
 echo "Total sum is $TOTAL1"
-echo "Fruits: $FRUITS[@]"
-echo "First fruit is: $FRUITS[0]"
-echo "First fruit is: $FRUITS[1]"
+echo "Fruits: ${FRUITS[@]}"
+echo "First fruit is: ${FRUITS[0]}"
+echo "First fruit is: ${FRUITS[1]}"
