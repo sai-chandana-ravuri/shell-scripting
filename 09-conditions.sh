9435725d164117a8d2494be76a341e81bcc4037a
sai-chandana-ravuri <saichandanaravuri@gmail.com>
2026-04-17 23:07:39 +0530

    code-changes

--- 09-conditions.sh
+++ 09-conditions.sh
@@ -3,9 +3,9 @@
 
 NUM=$1
 
-if [ $NUM -gt 30]; then
+if [ $NUM -gt 30 ]; then
    echo "Number is greater than 30"
-elif [ $NUM -lt 30]; then
+elif [ $NUM -lt 30 ]; then
    echo "Number is less than 30"
 else
    echo "Number is equal to 30"
