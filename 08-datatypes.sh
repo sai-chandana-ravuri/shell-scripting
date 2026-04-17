7bd3fdaca42416c4035ad4d621d5312da4101111
sai-chandana-ravuri <saichandanaravuri@gmail.com>
2026-04-17 22:42:00 +0530

    code-changes

--- 08-datatypes.sh
+++ 08-datatypes.sh
@@ -3,7 +3,7 @@
 NUM1=10
 NUM2=20
 NUM3=Chandana
-FRUITS=("Apple","Banana","Carrot")
+FRUITS=("Apple" "Banana" "Carrot")
 TOTAL=$(($NUM1+$NUM2))
 TOTAL1=$(($NUM1+$NUM2+$NUM3))
 echo "Total sum is $TOTAL"
