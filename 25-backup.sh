00807bc8462e3964f5576186d3ab03a9d3a785ea
sai-chandana-ravuri <saichandanaravuri@gmail.com>
2026-05-10 18:51:47 +0530

    code changes

--- 25-backup.sh
+++ 25-backup.sh
@@ -52,7 +52,7 @@ log "Source Directory: $SOURCE_DIR"
 log "Destination Directory: $DEST_DIR"
 log "Days: $DAYS"
 
-if [ -z $FILES]; then
+if [ -z $FILES ]; then
     log "No Files to archive ... $Y SKIPPING $N "
 else
     log "Files found to archieve: $FILES"
